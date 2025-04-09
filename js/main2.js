08a3a4fa9dc74889000d0384a770e890ccdd0c2c
emj <eduard.maju@gmail.com>
2025-04-09 16:25:19 -0600

    Cambios en el archivo main2.js

--- js/main2.js
+++ js/main2.js
@@ -3,10 +3,9 @@ let encabezado1 = document.getElementById("Saludo");
 if (localStorage.getItem("nombre") != null){
      let nombre = localStorage.getItem("nombre"); 
      encabezado1.innerText = `Hola, ${nombre} bienvenido/a de nuevo`;
- } else {
-     encabezado1.innerText = `Por favor ve al Index e ingresa tu nombre`;
  }
 
  btnEliminar.addEventListener("click", function(event) {
-    localStorage.clear();
+    localStorage.removeItem("nombre");
+    encabezado1.innerText = `Por favor ve al Index e ingresa tu nombre`;
 })
